a4622ca71a540fe176bc363af5561152decf873d
Mehdi Mehemel <mehdi.m_86@hotmail.fr>
2023-10-12 10:56:45 +0200

    React03 Pokedex

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,22 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+
 function PokemonCard() {
+    const pokemon = pokemonList[1];
     return (
-        
-            <figure>
-                <img src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-                <figcaption>Bulbasaur</figcaption>
-            </figure>
-    
-    )
+      <figure>
+        <figcaption> {pokemon.name} { pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p> } </figcaption>
+      </figure>
+    );
 }
+
 export default PokemonCard
