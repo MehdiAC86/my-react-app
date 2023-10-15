f7141df22cd712a92a2184ffd8c31ab5f9bc30dd
Mehdi Mehemel <mehdi.m_86@hotmail.fr>
2023-10-15 21:48:59 +0200

    Quête React 06 fini

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,60 @@
-import PokemonCard from "./components/PokemonCard"
+import { useState } from "react";
+import PokemonCard from "./components/PokemonCard";
+
+function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
 
 const pokemonList = [
   {
       name: "bulbasaur",
       imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
-  },
-];
-
-function App() {
-  return (
-    
-      <div>  
-      <PokemonCard pokemon ={pokemonList[0]} />
-      </div>
-   )
+    },
+  ];
+function pokeNext() {
+  setPokemonIndex(pokemonIndex + 1);
 }
+  
+  const pokePrev = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  }
 
+  return (
+    <>
+      <PokemonCard pokemon ={pokemonList[pokemonIndex]} />
+      {pokemonIndex > 0 ? (
+          <button onClick={pokePrev}>Précédent</button>
+      ) : (
+        ""
+      )}
+
+      {pokemonIndex < pokemonList.length - 1 ? (
+        <button onClick={pokeNext}>Suivant</button>
+        ) : (
+          ""
+        )}
+        </>
+  );
+        }
+
+      
+    
 export default App
